31ff5ff2fc219fbb2bf3251480b9491bc77f5d53
Porson <porsonlee@gmail.com>
2019-12-06 00:20:47 +0000

    added breedFetcher.js after refactoring. Collaborated with @prateek-diwedi

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,25 @@
 const request = require('request'); // when you request from http, you need to request again in line 4 with a link
-const breed = process.argv.slice(2);
+// const breedName = process.argv.slice(2);
+//const url = 
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
-  if (error) {
-    console.log('Failed to request breed', error);
-  }
-  if (body) {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('Breed is not defined.');
-    } else {
-      let breedSummary = data[0].description;
-      console.log(breedSummary);
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, function (error, response, body) {
+
+    if (body) {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        console.log('Breed is not defined.');
+      } else {
+        let breedSummary = data[0].description;
+        // console.log(breedSummary);
+        callback(error, breedSummary);
+      }
     }
-  }
-});
+  });
+
+
+};
+
+module.exports = { fetchBreedDescription };
