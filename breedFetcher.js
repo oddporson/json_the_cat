f1c1876e6a6ec437b18d9c81cff8f36a6315152c
Porson <porsonlee@gmail.com>
2019-12-06 00:23:04 +0000

    eslinted

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,11 @@
 const request = require('request'); // when you request from http, you need to request again in line 4 with a link
 // const breedName = process.argv.slice(2);
-//const url = 
+//const url =
 
 
 const fetchBreedDescription = function(breedName, callback) {
 
-  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, function (error, response, body) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, function(error, response, body) {
 
     if (body) {
       const data = JSON.parse(body);
